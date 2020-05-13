7aa51ae12fd8648cc0b8350783e987f32829cb98
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-13 16:24:35 -0400

    placing planner/calendar page

---
+++ client/src/pages/Calender/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Calendar(){
+
+return (
+    <div className="container">
+        <div className="alert alert-success" role="alert">
+            Calendar page 
+            </div>
+    </div>
+)
+
+}
+
+export default Calendar;
