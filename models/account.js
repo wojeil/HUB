70d0a645f664148f78642f8de4c3561a7f405dc6
Roger <rmartinez5926.rm@gmail.com>
2020-05-13 09:24:22 -0400

    add role to for accounts

--- models/account.js
+++ models/account.js
@@ -4,7 +4,8 @@ const passportLocalMongoose = require('passport-local-mongoose');
 
 const Account = new Schema({
     username: String,
-    password: String
+    password: String,
+    role: String
 });
 
 Account.plugin(passportLocalMongoose);
