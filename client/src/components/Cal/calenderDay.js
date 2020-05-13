adf5d3e7e60b103d0ae83332fe61a3b194cb2b93
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-13 11:35:42 -0400

    setting up calender components

---
+++ client/src/components/Cal/calenderDay.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import classnames from 'classnames';
+
+export default class CalendarDay extends React.Component {
+    render () {
+        const { day, isToday, events, onClick } = this.props;
+        const dayClasses = classnames({
+            'flexColumn': true,
+            'day': true,
+            'inactive': day.siblingMonth,
+            'today': isToday,
+        });
+
+        return (
+            <div 
+                onClick={onClick.bind(null, this, day)}
+                className={dayClasses}>
+                <div className="inner-grid">
+                    <div className="date">
+                        {day.day}
+                    </div>
+                    {events}
+                </div>
+            </div>
+        );
+    }
+}
+
+CalendarDay.propTypes = {
+    day: React.PropTypes.object.isRequired,
+    isToday: React.PropTypes.bool,
+    events: React.PropTypes.array,
+    onClick: React.PropTypes.func,
+};
+
+CalendarDay.defaultProps = {
+    onClick: () => {},
+}
