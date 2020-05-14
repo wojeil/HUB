961c28804e14aaf562c7861ae2991a4ee9b24812
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-14 11:57:51 -0400

    minor changes for dashboard set up

--- client/src/pages/Admin Dashboard/index.js
+++ client/src/pages/Admin Dashboard/index.js
@@ -5,33 +5,39 @@ import FormButton from "../../components/FormSubmitButton/index";
 function newPage() {
 
     return (
-      
-            <div className="container-fluid">
-                <div className="card" id="dashBoard">
-                    <Announcements title="" term="" body=""/>
-                    
+
+        <div className="container-fluid">
+            <div className="container" id="dashBoard">
+                <div className="card" id="shortTerm">
+                    <Announcements title="" term="" body="" />
+
                 </div>
-                <hr/>
-                <form>
-                    <div className="form-group">
-                        <label for="exampleFormControlInput1">Title</label>
-                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect1">Example select</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                            <option>Short-Term</option>
-                            <option>Long-Term</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <FormButton/>
-                </form>
+                <div className="card" id="longTerm">
+                    <Announcements title="" term="" body="" />
+
+                </div>
             </div>
-      
+            <hr />
+            <form>
+                <div className="form-group">
+                    <label for="exampleFormControlInput1">Title</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlSelect1">Example select</label>
+                    <select className="form-control" id="exampleFormControlSelect1">
+                        <option>Short-Term</option>
+                        <option>Long-Term</option>
+                    </select>
+                </div>
+                <div className="form-group">
+                    <label for="exampleFormControlTextarea1">Example textarea</label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <FormButton />
+            </form>
+        </div>
+
     )
 
 }
