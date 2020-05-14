f3ea3927c0b800f68e4f7cd367ca035b8cbe5276
Roger <rmartinez5926.rm@gmail.com>
2020-05-14 11:36:37 -0400

    update account model to contain a manager name

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -1,6 +1,9 @@
+// === Dependencies ===
 const Dashboard = require('../models/dashboard');
+const passport = require('passport');
 
 module.exports = {
+    // Get the 
     getDash: function (req, res) {
         Dashboard.findOne({owner:})
     }
