cdc5b6ec5de1b387a2a2c15b07b1abbf84212a8a
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-14 13:18:05 -0400

    setting up button form onclick

--- client/src/components/FormSubmitButton/index.js
+++ client/src/components/FormSubmitButton/index.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function FormButton (){
     return(
-        <button type="submit">Submit</button>
+        <button type="submit" 
+        onClick={() =>{
+
+        }}>Submit</button>
     )
 }
 
