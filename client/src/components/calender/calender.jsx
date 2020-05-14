4872ee949b1d2d360f69911f7407ea1be3c9dff2
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-14 13:26:22 -0400

    loop for the weekdays

--- client/src/components/calender/calender.jsx
+++ client/src/components/calender/calender.jsx
@@ -27,7 +27,19 @@ class CalendarTwo extends React.Component {
           </div>
         );
       }
-      renderDays() {}
+      renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+        for (let i = 0; i < 7; i++) {
+          days.push(
+            <div className="col col-center" key={i}>
+              {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+            </div>
+          );
+        }
+        return <div className="days row">{days}</div>;
+      }
       renderCells() {}
       onDateClick = day => {};
       nextMonth = () => {
