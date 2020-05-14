029bce4faa5204068b3ea67d72e02c443f1e568b
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-05-14 12:13:18 -0400

    calender component and render

---
+++ client/src/components/calender/calender.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import dateFns from "date-fns";
+
+class Calendar extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Header</div>
+        <div>Days</div>
+        <div>Cells</div>
+      </div>
+    );
+  }
+}
+
+export default Calendar;
--- client/src/pages/Calender/index.js
+++ client/src/pages/Calender/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import CalendarTwo from '../../components/calender/calender.jsx'
+class Calendar extends React.Component(){
 
-function Calendar(){
-
-return (
-    <div className="container">
-        <div className="alert alert-success" role="alert">
-            Calendar page 
-            </div>
-    </div>
-)
+    render() {
+        return (
+          <div className="App">
+            <header>
+              <div id="logo">
+                <span className="icon">date_range</span>
+                <span>
+                  react<b>calendar</b>
+                </span>
+              </div>
+            </header>
+            <main>
+              <CalendarTwo />
+            </main>
+          </div>
+        );
+      }
 
 }
 
