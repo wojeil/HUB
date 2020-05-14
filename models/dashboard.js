43baf5fc42cd3a3cee05c1d94b245e2386e8405a
Roger <rmartinez5926.rm@gmail.com>
2020-05-14 11:05:39 -0400

    fill out dashboard schema

--- models/dashboard.js
+++ models/dashboard.js
@@ -2,7 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Dashboard = new Schema({
-    
+    items:[
+        {
+           title: String,
+           type: String,
+            content: String
+        }
+    ],
+    owner: {
+        type:Schema.Types.ObjectId , ref: 'accounts'
+    },
+    lastUpdated: Date
 });
 
 module.exports = mongoose.model('dashboards', Dashboard);
