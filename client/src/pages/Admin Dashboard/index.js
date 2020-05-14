9c93ca5d305c48397d7d2972fc491f831de2b614
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-14 10:50:11 -0400

    form interface for admin dash

--- client/src/pages/Admin Dashboard/index.js
+++ client/src/pages/Admin Dashboard/index.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
-function newPage(){
+function newPage() {
 
-return (
-    <div className="container">
-        <div className="alert alert-success" role="alert">
-            Success, You are admin
+    return (
+        <>
+            <div className="container-fluid">
+                <div className="alert alert-success" role="alert">
+                    Success, You are admin
             </div>
-    </div>
-)
+
+                <form>
+                    <div className="form-group">
+                        <label for="exampleFormControlInput1">Title</label>
+                        <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Title" />
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlSelect1">Example select</label>
+                        <select className="form-control" id="exampleFormControlSelect1">
+                            <option>Short-Term</option>
+                            <option>Long-Term</option>
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
+            </div>
+        </>
+    )
 
 }
 
