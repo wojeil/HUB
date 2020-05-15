4adc29cb9996273f694a1720232b259bbc6b27aa
Roger <rmartinez5926.rm@gmail.com>
2020-05-15 08:00:51 -0400

    add a query to add to the items array

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -6,25 +6,35 @@ module.exports = {
     // Get the dash for current user
     getDash: function (req, res) {
         const { user } = req.session.passport
-        
-        Account.findOne({username:user}).then(userData => {
+
+        Account.findOne({ username: user }).then(userData => {
             console.log(userData);
             Dashboard.findOne({ owner: userData.manager })
-            .then(dashData => {
-                console.log(dashData)
-                res.json(dashData)
-            })
+                .then(dashData => {
+                    console.log(dashData)
+                    res.json(dashData)
+                })
         })
-        
+
     },
 
     // Create new Dashboard
     postDash: function (req, res) {
         const { user } = req.session.passport
-        Dashboard.create({owner:user, lastUpdated: Date.now()}, (err, data) =>{
-            if(err) throw err;
+        Dashboard.create({ owner: user, lastUpdated: Date.now() }, (err, data) => {
+            if (err) throw err;
             res.status(200);
         })
     },
-    
+    // Create item in Dashboard
+    addDash: function (req, res) {
+        const { user } = req.session.passport
+        Account.findOne({ username: user }).then(userData => {
+            Dashboard.findOneAndUpdate({ owner: user}, {lastUpdated: Date.now(), $push : {items: req.body} } , (err, data) => {
+                if (err) throw err;
+                res.status(200);
+            })
+        })
+    },
+
 }
