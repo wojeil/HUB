8876c56957a7fd60fab2905a31568874f88d4aad
Roger <rmartinez5926.rm@gmail.com>
2020-05-15 07:29:34 -0400

    create routes to add and remove items from dash

--- routes/api/dash.js
+++ routes/api/dash.js
@@ -6,5 +6,12 @@ router.route("/get")
   .get(dashController.getDash);
 
 router.route("/post")
-  .post(dashController.postDash)
+  .post(dashController.postDash);
+
+router.route("/add")
+  .post(dashController.addDash);
+
+router.route("/remove")
+  .post(dashController.removeDash);
+
 module.exports = router;
