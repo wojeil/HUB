00614a19231f87f4b215d66bc919de612d0f07ef
Roger <rmartinez5926.rm@gmail.com>
2020-05-17 14:37:14 -0400

    crete route to get plans

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,6 +1,14 @@
 const Plan = require("../models/plan");
 
 module.exports = {
+    // Get plans for user
+    getPlan: function (req,res) {
+        const { user } = req.session.passport;
+        Plan.find({ owner: user }).then(plans => {
+            res.status(200).json(plans);
+        });
+    },
+    //Create a new plan
     addPlan: function (req, res) {
         const { user } = req.session.passport;
         const {day, time, plan} = req.body
@@ -12,7 +20,7 @@ module.exports = {
          }, (err) => {
             if (err) throw err;
             res.status(200);
-        })
+        });
 
     }
 }
