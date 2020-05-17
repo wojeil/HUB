fd435425a7898daea73c8448b2b569288030f5d5
Roger <rmartinez5926.rm@gmail.com>
2020-05-17 14:39:22 -0400

    put in route to get plans

--- routes/api/plan.js
+++ routes/api/plan.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const planController = require("../../controllers/planController");
 
 // Matches with "/api/plan"
-
+router.route("/get")
+    .get(planController.getPlan);
 router.route("/add")
   .post(planController.addPlan);
 
