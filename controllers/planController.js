9ac0928938516d865ea43345bdd083c3b3d25a42
Roger <rmartinez5926.rm@gmail.com>
2020-05-17 14:21:13 -0400

    add control to add a plan to the db

--- controllers/planController.js
+++ controllers/planController.js
@@ -1,2 +1,18 @@
 const Plan = require("../models/plan");
 
+module.exports = {
+    addPlan: function (req, res) {
+        const { user } = req.session.passport;
+        const {day, time, plan} = req.body
+        Plan.create({
+            day,
+            time,
+            plan,
+            owner: user
+         }, (err) => {
+            if (err) throw err;
+            res.status(200);
+        })
+
+    }
+}
