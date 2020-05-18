1f16035d7fcb578a1eb29b9e815a19f03ac32445
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-18 18:04:41 -0400

    added delete button component

---
+++ client/src/components/ClearAllButton/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+function ClearAllButton (){
+
+    return (
+        <button className="btn" role= "button"> Clear All </button>
+    )
+}
+
+export default ClearAllButton
