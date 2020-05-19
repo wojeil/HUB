ad3e9bcb365ec228048b1156b0c38b011f92612c
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-19 15:39:05 -0400

    working on getting user's from Team

--- client/src/components/PlannerSearch/index.js
+++ client/src/components/PlannerSearch/index.js
@@ -1,17 +1,49 @@
-import React from "react"
-import {Container} from "../Grid"
+import React, {useEffect, useState} from "react";
+import { Container } from "../Grid"
 import Card from "../Card"
 import TeamMates from "../TeamMates"
+import API from "../../utils/API"
 
 
-function PlannerSearch (){
+function PlannerSearch() {
 
-    return(
-        
+    const [users, setUser] = useState("")
+    
+    
+    useEffect(() => {
+        loadPlans();
+    },[])
+
+    function loadPlans() {
+
+        API.getUser()
+            .then(res => {
+                // setUser(res.data[0].schedule);
+                console.log("user", res);
+            }
+            ).then(() => {
+                console.log("the user", users);
+            })
+            .catch(err => console.log(err));
+    };
+
+    return (
+
         <Container>
-            <TeamMates/>
+            <TeamMates />
             <Card title="Find Your TeamMate's Planner">
-                
+            <form>
+                <div className="form-group">
+                    <label for="exampleFormControlSelect1">Example select</label>
+                    <select className="form-control" id="exampleFormControlSelect1">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                </div>
+            </form>
             </Card>
         </Container>
     )
