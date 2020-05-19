862bc363f8739ded6c86fb43a81bf5479486b5bd
Roger <rmartinez5926.rm@gmail.com>
2020-05-19 12:56:33 -0400

    add route to search for team

--- routes/api/search.js
+++ routes/api/search.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const searchController  = require("../../controllers/searchController");
 // Matches with "/api/search"
-
+router.route("/team")
+.get(searchController.getTeam);
 module.exports = router;
