3b6510b7ce032a8bcd513918acd84219f62b1a63
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-19 13:05:06 -0400

    all pages set,,route for planner search is set

--- client/src/pages/PlannerSearchPage/index.js
+++ client/src/pages/PlannerSearchPage/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import {Container,Row,Col} from "../../components/Grid"
-
+import PlannerSearch from "../../components/PlannerSearch"
 
 function PlannerSearchPage (){
 
     return(
         <Container>
-
+        
+        <PlannerSearch/>
 
         </Container>
 
