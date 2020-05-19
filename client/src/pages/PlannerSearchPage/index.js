86e49a0fa48e197768bfa025315e83be0c7c72b1
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-05-19 12:49:36 -0400

    page and planner search comp created

---
+++ client/src/pages/PlannerSearchPage/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {Container,Row,Col} from "../../components/Grid"
+
+
+function PlannerSearchPage (){
+
+    return(
+        <Container>
+
+
+        </Container>
+
+    )
+}
+
+export default PlannerSearchPage
