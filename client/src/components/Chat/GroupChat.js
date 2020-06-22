0847d06bcaf43b09f86ad804fbc4808516a4674f
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-06-22 19:00:12 -0400

    changed port

--- client/src/components/Chat/GroupChat.js
+++ client/src/components/Chat/GroupChat.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import ChatInput from './ChatInput'
 import ChatMessage from './ChatMessage'
 
-const URL = 'ws://localhost:3030'
+const URL = 'ws://localhost:3000'
 
 class GroupChat extends Component {
   state = {
@@ -14,19 +14,16 @@ class GroupChat extends Component {
 
   componentDidMount() {
     this.ws.onopen = () => {
-      // on connecting, do nothing but log it to the console
       console.log('connected')
     }
 
     this.ws.onmessage = evt => {
-      // on receiving a message, add it to the list of messages
       const message = JSON.parse(evt.data)
       this.addMessage(message)
     }
 
     this.ws.onclose = () => {
       console.log('disconnected')
-      // automatically try to reconnect on connection loss
       this.setState({
         ws: new WebSocket(URL),
       })
@@ -37,7 +34,6 @@ class GroupChat extends Component {
     this.setState(state => ({ messages: [message, ...state.messages] }))
 
   submitMessage = messageString => {
-    // on submitting the ChatInput form, send the message, add it to the list and reset the input
     const message = { name: this.state.name, message: messageString }
     this.ws.send(JSON.stringify(message))
     this.addMessage(message)
